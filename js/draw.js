79d1d925dab1c309b314337556db3548ff2fdf48
Chetabahana <chetabahana@gmail.com>
2019-10-03 12:57:45 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -77,7 +77,7 @@ var draw = {
                     ]);
 
                     $('#type')[0].href = 'GoJS/api/symbols/Diagram.html#makeSvg';
-                    var svg = diagram.makeSvg({scale: 2});
+                    var svg = diagram.makeSvg({scale: 0.5});
                     $('.diagram').append(svg);
 
                 }
