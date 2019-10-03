77d9a1cac9c94441b964260aba683512e6b474e0
Chetabahana <chetabahana@gmail.com>
2019-10-04 01:55:00 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -11,11 +11,11 @@ editor.getSession().on('change', _.debounce(function() {draw.diagram();}, 100) )
 var draw = {
 
     scope : [
-        {
-           color : 'blue',
-           size : 'large',
-           type : 'mug'
-        }
+        sequence  : 'sequence/js/sequence-diagram-snap-min.js',
+        flowchart : 'flowchart/flowchart-latest.js',
+        railroad  : 'railroad/railroad-diagrams.js',
+        nodelinks : 'nodelinks/release/go.js',
+        scenetree : 'graphql/build.js'
     ],
 
     'sequence' : 'sequence/js/sequence-diagram-snap-min.js',
