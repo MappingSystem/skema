00139e043f31d9aba7c5c848f30c29d9e5314beb
Chetabahana <chetabahana@gmail.com>
2019-10-07 01:41:39 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -210,10 +210,11 @@ var draw = {
 
         var kinds = draw.kind[0];
         var index = 0; for (key in kinds) {if(key == draw.type) nIndex = index; index++;}
-        //$(".theme").val("simple"); draw.tChange(); draw.svg[draw.type] = $('svg').get(0);
 
         var n = ['0', '00', '99', '000', '999', '0000', '9999', '00000', '99999'].includes(this.id);
         var itemIndex = (n)? ((nIndex == 0)? index - 1 : nIndex - 1): ((nIndex + 1 == index)? 0: nIndex + 1);
+
+       //$(".theme").val("simple"); draw.tChange(); draw.svg[draw.type] = $('svg').get(0);
         draw.type = _.findKey(kinds, function(item) {return _.indexOf(Object.values(kinds), item) == itemIndex;});
 
         var jsonfile = '/assets/feed.json?t=' + $.now();
