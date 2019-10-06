bb2982dd360c79a8f55677a53a9d5a6e52a17c96
Chetabahana <chetabahana@gmail.com>
2019-10-07 00:46:36 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -206,7 +206,7 @@ var draw = {
         }
     },
 
-     elClick : function() {
+    elClick : function() {
 
         var kinds = draw.kind[0];
         var index = 0; for (key in kinds) {if(key == draw.type) nIndex = index; index++;}
