b2fe187887d5044a386f85047ccdf216da29b2dc
Chetabahana <chetabahana@gmail.com>
2019-10-07 01:38:28 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -201,23 +201,23 @@ var draw = {
 
             draw.elements.each(function() {
                 this.parentNode.appendChild(this);
-            }).css({'cursor':'pointer'}).click(function() {draw.elClick();});
+            }).css({'cursor':'pointer'}).click(function(e) {draw.elClick($(this));});
 
         }
     },
 
-    elClick : function() {
+    elClick : function(this) {
 
         var kinds = draw.kind[0];
         var index = 0; for (key in kinds) {if(key == draw.type) nIndex = index; index++;}
         //$(".theme").val("simple"); draw.tChange(); draw.svg[draw.type] = $('svg').get(0);
 
-        var n = ['0', '00', '99', '000', '999', '0000', '9999', '00000', '99999'].includes($(this).id);
+        var n = ['0', '00', '99', '000', '999', '0000', '9999', '00000', '99999'].includes(this.id);
         var itemIndex = (n)? ((nIndex == 0)? index - 1 : nIndex - 1): ((nIndex + 1 == index)? 0: nIndex + 1);
         draw.type = _.findKey(kinds, function(item) {return _.indexOf(Object.values(kinds), item) == itemIndex;});
 
         var jsonfile = '/assets/feed.json?t=' + $.now();
-        jsonfile = jsonfile.replace('assets', $(this).id);
+        jsonfile = jsonfile.replace('assets', this.id);
         $("#json").attr("href", jsonfile);
 
         $.getJSON(jsonfile).done(function(result){
