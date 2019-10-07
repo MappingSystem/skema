8accbf0d1daa8dcd41d1a0ad21d61774f9ca20d3
Chetabahana <chetabahana@gmail.com>
2019-10-07 15:19:27 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -198,41 +198,37 @@ var draw = {
 
             }
 
-            draw.elements.css({'cursor':'pointer'})
+            draw.elements.css({'cursor':'pointer'}).each(function() {
+                this.parentNode.appendChild(this);}).click(function() {draw.elClick(this.id);}
+            );
+        }
+    },
 
-            .each(function() {
 
-                this.parentNode.appendChild(this);
+    elClick : function(id) {
 
-            })
+        var kinds = draw.kind[0];
+        draw.svg[draw.type] = $('svg').get(0);
+        var index = 0; for (key in kinds) {if(key == draw.type) nIndex = index; index++;}
 
-            .click(function() {
+        var n = ['0', '00', '99', '000', '999', '0000', '9999', '00000', '99999'].includes(id);
+        var itemIndex = (n)? ((nIndex == 0)? index - 1 : nIndex - 1): ((nIndex + 1 == index)? 0: nIndex + 1);
+        draw.type = _.findKey(kinds, function(item) {return _.indexOf(Object.values(kinds), item) == itemIndex;});
 
-                var kinds = draw.kind[0];
-                draw.svg[draw.type] = $('svg').get(0);
-                var index = 0; for (key in kinds) {if(key == draw.type) nIndex = index; index++;}
 
-                var n = ['0', '00', '99', '000', '999', '0000', '9999', '00000', '99999'].includes(this.id);
-                var itemIndex = (n)? ((nIndex == 0)? index - 1 : nIndex - 1): ((nIndex + 1 == index)? 0: nIndex + 1);
-                draw.type = _.findKey(kinds, function(item) {return _.indexOf(Object.values(kinds), item) == itemIndex;});
+        var jsonfile = '/assets/feed.json?t=' + $.now();
+        jsonfile = jsonfile.replace('assets', id);
+        $("#json").attr("href", jsonfile);
 
- 
-                var jsonfile = '/assets/feed.json?t=' + $.now();
-                jsonfile = jsonfile.replace('assets', this.id);
-                $("#json").attr("href", jsonfile);
+        $.getJSON(jsonfile).done(function(result){
 
-                $.getJSON(jsonfile).done(function(result){
+            var obj = result.items[4].items[itemIndex];
+            draw.input = obj.input; draw.skema = draw.encode(obj.query);
+            if(itemIndex != index - 1) editor.setValue(draw.skema);
+            else {$(".theme").val("simple"); draw.tChange();}
 
-                    var obj = result.items[4].items[itemIndex];
-                    draw.input = obj.input; draw.skema = draw.encode(obj.query);
-                    if(itemIndex != index - 1) editor.setValue(draw.skema);
-                    else {$(".theme").val("simple"); draw.tChange();}
+        });
 
-                });
-
-            });
-
-        } 
     },
 
     xmlData : function() {
