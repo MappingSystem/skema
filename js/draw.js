5f9526e7ff83669434764b43270d6be3898737bb
Chetabahana <chetabahana@gmail.com>
2019-10-10 22:28:16 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -190,7 +190,7 @@ var draw = {
         e.id = draw.pad(i, 2);
         e.parentNode.appendChild(e);
         $(e).css({'cursor':'pointer'});
-        $(e).filter('.title, .actor, .signal').hover(show(100), hide(100));
+        $(e).filter('.title, .actor, .signal').hover(function() {$(this).hide(100).show(100);});
         $(e).mouseenter(function(){$(this).css('fill','teal')}).mouseout(function(){$(this).css('fill','')});
         if ($(e).hasClass('execute-button')) $('body').on('DOMSubtreeModified', '.CodeMirror', function() {draw.button();});
 
