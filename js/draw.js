102fd3df752ab90698d8e7ca4af3a2b5f7de8089
Chetabahana <chetabahana@gmail.com>
2019-10-10 09:14:05 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -176,8 +176,7 @@ var draw = {
 
         var regex = /[?&]([^=#]+)=([^&#]*)/g, url = window.location.href, params = {}, match;
         while(match = regex.exec(url)) {params[match[1]] = match[2];}
-        draw.params = params; console.log(draw.params);
-
+        draw.params = params;
         draw.diagram();
 
     },
