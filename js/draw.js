a12ad334ee87a27f8676b36ccc97ae5b2fdf4a12
Chetabahana <chetabahana@gmail.com>
2019-10-10 18:11:14 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -95,7 +95,8 @@ var draw = {
             else if (type == 'railroad') {elements = $('svg path').first().add($('svg rect')).add($('svg path').last());}
             else if (type == 'nodelinks') {elements = $('svg g g g');}
             //else if (type == 'scenetree') {$('.CodeMirror-code').change(function() {draw.change(this);});}
-            else if (type == 'scenetree') {elements = $('button.execute-button');}
+            //else if (type == 'scenetree') {elements = $('button.execute-button');}
+            else if (type == 'scenetree') {elements = $('svg path');}
             if(elements) elements.each(function(index) {draw.node(index, this);}).click(function() {draw.click(this);});
 
         } 
