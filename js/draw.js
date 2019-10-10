b7868a63e8d95afc2876d625fef2138418c44977
Chetabahana <chetabahana@gmail.com>
2019-10-10 18:21:59 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -65,7 +65,7 @@ var draw = {
                 else if(type == 'flowchart') {diagram = flowchart.parse(skema); diagram.drawSVG(g, input);}
                 else if(type == 'railroad') {diagram = eval(skema).format(input); diagram.addTo(g);}
                 else if(type == 'nodelinks') {diagram = draw.makeSvg(input, skema); g.prepend(diagram);}
-                //else if(type == 'scenetree') {diagram = d3.select(".diagram"); g.prepend(draw.svg['sequence']);}
+                else if(type == 'scenetree') {diagram = d3.select('#viewport');}
 
             } finally {
 
