03b7d79257800817cdefba2c959ffb4b1a7f8b7b
Chetabahana <chetabahana@gmail.com>
2019-10-10 23:04:22 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -94,7 +94,7 @@ var draw = {
             else if (type == 'flowchart') {elements = $('svg rect.flowchart, svg path.flowchart');} 
             else if (type == 'railroad') {elements = $('svg path').first().add($('svg rect')).add($('svg path').last());}
             else if (type == 'nodelinks') {elements = $('svg g g g').hover(function() {$(this).hide(100).show(100);});}
-            else if (type == 'scenetree') {elements = $('button.execute-button svg path').prop('class', 'exec-query');};
+            else if (type == 'scenetree') {elements = $('button.execute-button svg path').attr('class', 'exec-query');};
             elements.each(function(index) {draw.node(index, this);}).click(function() {draw.click(this);});
 
         } 
