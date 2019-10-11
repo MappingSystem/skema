5b3f2386b9cfce27dc69723d10db4edfbeda8026
Chetabahana <chetabahana@gmail.com>
2019-10-11 08:20:47 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -168,7 +168,7 @@ var draw = {
 
         if (!draw.test) {
             var result = "{" + $('#graphiql .resultWrap').text().split("{").pop();
-            if (result.isJSON()) {draw.test = !draw.test; console.log(result);}
+            if (result.isJSON()) {draw.test = !draw.test; draw.click($('.eQuery'));}
         }
 
     },
