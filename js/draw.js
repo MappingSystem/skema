631891300d7701085ae9cf68a4d3f490ca308d5d
Chetabahana <chetabahana@gmail.com>
2019-10-11 16:09:02 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -195,7 +195,7 @@ var draw = {
         $(e).css({'cursor':'pointer'});
         $(e).filter('.title, .actor, .signal').hover(function() {$(this).hide(100).show(100);});
         $(e).mouseenter(function(){$(this).css('fill','teal')}).mouseout(function(){$(this).css('fill','')});
-        //if ($(e).attr('class') == 'eQuery') $('body').on('DOMSubtreeModified', '.resultWrap', function() {draw.query();});
+        if ($(e).attr('class') == 'eQuery') $(e).clone().appendTo($(e).parent());
 
     },
 
