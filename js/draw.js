ac5b7001e01d718f72034f8c023b8e26d8ff6bd6
Chetabahana <chetabahana@gmail.com>
2019-10-11 20:32:01 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -202,7 +202,7 @@ var draw = {
     clone : function(e) {
 
         var button = $('button.execute-button').clone();
-        $('button.execute-button').find('svg path').css({'pointer-events': 'none'});
+        $('button.execute-button').find('svg path').css({'pointer-events': 'auto'});
         button.prependTo($('button.execute-button').parent()).css({'pointer-events': 'none'});
         button.find('svg path').css({'transform':'rotate(180deg)','transform-origin':'48% 48%'});
 
