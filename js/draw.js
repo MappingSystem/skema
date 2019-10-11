62e4885d8ed3dc03c648c6e7f906f9a2994b47ff
Chetabahana <chetabahana@gmail.com>
2019-10-11 14:45:31 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -169,6 +169,7 @@ var draw = {
 
     isJSON : function(str) {
 
+        if (str == "{" ) return false;
         if ( /^\s*$/.test(str)) return false;
         str = str.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@');
         str = str.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']');
