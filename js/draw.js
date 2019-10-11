323729f5c5d9d7b32c04cd0a9065cac11114e8bb
Chetabahana <chetabahana@gmail.com>
2019-10-11 22:20:57 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -73,7 +73,7 @@ var draw = {
             } finally {
 
                 draw.type = type; draw.element();
-                $('.eQuery#01').click(false);
+                $('.eQuery#01').click(function(){return false;});
                 $('.loadingImg').hide();
 
             }
