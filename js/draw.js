cd3bcb1d02df3fd46dda5f33b903c0f125264ca1
Chetabahana <chetabahana@gmail.com>
2019-10-12 13:18:29 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -102,7 +102,7 @@ var draw = {
             //set handle with idle time of user inactivity
             elements.each(function(index) {draw.node(index, this);})
             if (type != 'scenetree') {elements.click(function() {draw.click(this);});}
-            $('body').on('click mousemove keyup', _.debounce(function(){draw.reload();}, 60000));
+            $('body').on('click mousemove keyup', _.debounce(function(){draw.reload('#chetabahana-skema');}, 60000));
 
         }
 
@@ -227,11 +227,11 @@ var draw = {
 
     },
 
-    reload : function() {
+    reload : function(hash) {
 
-        window.location.hash = "#chetabahana-skema";
-        window.location.reload(true);
         window.stop();
+        window.location.hash = hash;
+        window.location.reload(true);
 
     },
 
