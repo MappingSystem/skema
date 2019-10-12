f2b0b059d88b2ad7a93c4c2716d770c218ed37b4
Chetabahana <chetabahana@gmail.com>
2019-10-12 14:53:12 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -230,7 +230,7 @@ var draw = {
     reload : function(hash) {
 
         var target = $("a").filter(function() {return this.hash == hash}); target.click();
-        window.stop(); window.location.hash = hash; window.location.reload(true);
+        window.location.hash = hash; window.location.reload(true);
 
     },
 
