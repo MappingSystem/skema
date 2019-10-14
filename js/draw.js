c4922fc3001883b1e8ebea55444b9f8aa85800b0
Chetabahana <chetabahana@gmail.com>
2019-10-14 14:57:46 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -30,7 +30,7 @@ var js, json, link, type, test, input, skema, select, draw = {
 
                 $('#tautan a').each(function(value, key){
                     if (select == 'hand') {
-                        this.href = link.nth-child(key+1)().href;
+                        this.href = link.slice(key,key+1).href;
                         $(this).css({'cursor':'pointer'});
                     } else {
                         if (item[this.id]) {this.href = item[this.id];}
