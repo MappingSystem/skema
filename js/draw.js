b16d91aa43cb2a210ba85e04812fde2e6704c052
Chetabahana <chetabahana@gmail.com>
2019-10-26 14:25:21 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -142,15 +142,7 @@ var js, pad, feed, json, init, link, size, test, type, style, skema, select, par
         pad = (n)? ((pad == 0)? size - 1 : pad - 1): ((pad + 1 == size)? 0: pad + 1);
         feed = feed.replace('assets', $(e).attr("id"));
         type = json[pad]['title'];
-
-        //Get json address of skema
-        $.getJSON(feed).done(function(result){
-
-            //Display link on success
-            $("#json").attr("href", feed);
-            var obj = result.items[4].items[pad];
-            style = obj.input.style; skema = obj.input.skema;
-            editor.setValue(draw.encode(JSON.stringify(skema, null, 4)));
+        draw.getJSON();
 
         });
 
@@ -189,7 +181,7 @@ var js, pad, feed, json, init, link, size, test, type, style, skema, select, par
     getJSON : function() {
 
         //Inject Workflows from getJSON
-        feed = '/feed.json?t=' + $.now();
+        if (!feed) feed = '/feed.json?t=' + $.now();
         $.getJSON(feed).done(function(result){
 
             if (!json) json = result.items[4].items;
@@ -198,8 +190,18 @@ var js, pad, feed, json, init, link, size, test, type, style, skema, select, par
             if (!link) link = $('#tautan a').clone();
             if (!type) type = 'Sequence';
 
-            if (skema) {editor.setValue(skema);}
-            else {draw.diagram();}
+            if (!pad) {
+
+                draw.diagram();
+
+            } else {
+ 
+                //Display link on success
+                $("#json").attr("href", feed);
+                style = json[pad].input.style; skema = json[pad].input.skema;
+                editor.setValue(draw.encode(JSON.stringify(skema, null, 4)));
+
+            }
 
         });
 
