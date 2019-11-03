1c233576733e1bb45534310d856ce11a34a0316c
Chetabahana <chetabahana@gmail.com>
2019-11-03 20:57:52 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -122,7 +122,7 @@ var id, js, ids, pad, back, feed, json, init, link, size, test, type, style, ske
             else if (type == 'Flowchart') {elements = $('svg rect.flowchart, svg path.flowchart');}
             else if (type == 'Railroad') {elements = $('svg path').first().add($('svg rect')).add($('svg path').last());}
             else if (type == 'Nodelinks') {elements = $('svg g g g').hover(function() {$(this).hide(100).show(100);});}
-            else if (type == 'Sitewheel') {elements = $('svg line.link, svg g.node, svg title');}
+            else if (type == 'Sitewheel') {elements = $('svg line.link, svg g.node');}
             else if (type == 'Scenetree') {draw.clone(); elements = $('button svg path').attr('class','eQuery');};
 
             //set each id and its handle 
