aaf79947db16f15a512c794e27a96a16d9acfc2e
Chetabahana <chetabahana@gmail.com>
2019-11-05 02:54:46 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -88,7 +88,7 @@ var id, js, ids, pad, back, feed, json, init, link, size, test, type, style, ske
                 //Support Skema with all diagram types including ones from GraphiQL/Threejs/D3 
                 if (type == 'Sitewheel') {initialize(skema).then (function (control) {doTheTreeViz(control);});}
                 else if (type == 'Flowchart') {diagram = flowchart.parse(skema); diagram.drawSVG(g, style);}
-                else if (type == 'Railroad') {main.drawDiagramsFromSerializedGrammar(skema, $("#diagrams"));}
+                else if (type == 'Railroad') {main.drawDiagramsFromSerializedGrammar(skema, g);}
                 else if (type == 'Sequence') {diagram = Diagram.parse(skema); diagram.drawSVG(g, style);}
                 else if (type == 'Nodelinks') {diagram = draw.makeSvg(); g.prepend(diagram);}
                 else if (type == 'Scenetree') {diagram = d3.select('#viewport');}
