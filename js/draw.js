64eaab023cc22e7a2950b2879aaa7965e5aad5af
Chetabahana <chetabahana@gmail.com>
2019-11-08 09:14:54 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -296,8 +296,8 @@ var id, js, ids, pad, back, feed, json, init, link, size, test, type, style, ske
     },
 
     feed : function() {
-var scope = {["part"]};
-        if (typeof part !== "undefined") {feed = part.feed(id, size); draw.getJSON();}
+
+        if (typeof {["part"]}.feed !== "undefined") {feed = part.feed(id, size); draw.getJSON();}
         else {$.getScript('skema/js/part.js', function() {draw.feed();});}
 
     },
