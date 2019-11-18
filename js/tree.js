df773c61b71dfba6e643a8464da51f558bc8ad4b
Chetabahana <chetabahana@gmail.com>
2019-11-19 01:35:06 +0700

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -2,6 +2,8 @@ var tree = {
 
     feed : function(id, size) {
 
+        //https://szhshp.org/tech/2019/08/10/reactindepthrender.html
+        console.dir(document.getElementById('graphiql')._reactRootContainer._internalRoot);
         draw.getJSON();
 
     }
