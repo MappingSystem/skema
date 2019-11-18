6e959b73d66ed954d022db845278f1408f4e00db
Chetabahana <chetabahana@gmail.com>
2019-11-19 00:04:56 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -126,8 +126,8 @@ var id, js, ids, pad, back, data, feed, json, link, size, test, type, query, sel
         var cm = $('#graphiql .queryWrap .CodeMirror')[0];
 
         var x = ($('#diagram').is(':visible') && !$('#diagram').find('svg')[0])? true: false;
-        var y = ($('#graphiql').css('visibility') === 'visible' && !cm.length)? true: false;
-        var z = false; if (cm.length) if (!(cm.CodeMirror instanceof Object)) var z = true;
+        var y = ($('#graphiql').css('visibility') === 'visible' && !cm)? true: false;
+        var z = false; if (cm) if (!(cm.CodeMirror instanceof Object)) var z = true;
 
         if (x || y || z) {
 
