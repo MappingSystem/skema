4c219333fd0a999820252463a807fea3a3fb32bc
Chetabahana <chetabahana@gmail.com>
2019-11-18 10:47:40 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -127,7 +127,7 @@ var id, js, ids, pad, back, data, feed, json, link, size, test, type, select, pa
         if (
            ($('#diagram').is(':visible') && !$('#diagram').find('svg')[0]) || 
            ($('#graphiql').css('visibility') && !$('#graphiql .queryWrap .CodeMirror')[0]) || 
-           ($('#graphiql').css('visibility') && $('.CodeMirror')[0] && !$('.CodeMirror')[0].CodeMirror instanceof Object)
+           //($('#graphiql').css('visibility') && $('.CodeMirror')[0] && !$('.CodeMirror')[0].CodeMirror instanceof Object)
         ){
 
             window.requestAnimationFrame(draw.element);
