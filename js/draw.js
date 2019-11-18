45b731b05f8546688dc334181453357e5f5bdb4f
Chetabahana <chetabahana@gmail.com>
2019-11-18 07:18:36 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -124,7 +124,7 @@ var id, js, ids, pad, back, data, feed, json, link, size, test, type, select, pa
     element : function() {
  
         if ((!$('#diagram').find('svg')[0] && $('#diagram').is(':visible')) || 
-           (!$('#graphiql').find('svg')[0] && $('#graphiql').is(':visible'))) {
+           (!$('#graphiql').find('svg')[0] && $('#graphiql').css('visibility') === 'visible')) {
 
             window.requestAnimationFrame(draw.element);
 
