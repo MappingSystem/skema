dc83a4907f2ef936157ca54a8ae14c11805a199d
Chetabahana <chetabahana@gmail.com>
2019-11-20 05:43:28 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -281,10 +281,8 @@ var id, js, ids, pad, back, data, feed, json, link, size, test, type, query, sel
 
     name : function(value) {
 
-        if (typeof value === 'string') return value.replace(' mypointer', '') + ' mypointer';
-else return 'mypointer';
-        //if (!value || typeof value !== 'string' || !(value instanceof String)) return 'mypointer';
-        //else return (value).replace(' mypointer', '') + ' mypointer';
+        if (typeof value !== 'string') return 'mypointer';
+        else return value.replace(' mypointer', '') + ' mypointer';
 
     },
 
