d7713ba8dc5146904df81a8588d200130f764fae
Chetabahana <chetabahana@gmail.com>
2019-11-21 03:09:40 +0700

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -2,15 +2,15 @@ var tree = {
 
     feed : function(id, size) {
 
-        // Accessing React Internal Instance
+        // Accessing _internalRoot
         // https://szhshp.org/tech/2019/08/10/reactindepthrender.html
         // https://dev.to/carlmungazi/a-journey-through-reactdom-render-302c
 
-        var graphiql = document.getElementById('graphiql');
-        console.log(tree.getReact(graphiql));
-
         var root = graphiql._reactRootContainer._internalRoot;
         console.dir(root.current);
+
+        var graphiql = document.getElementById('graphiql');
+        console.log(tree.getReact(graphiql));
 
         draw.getJSON();
 
@@ -18,7 +18,7 @@ var tree = {
 
     getReact : function(dom) {
 
-        // Accessing global element state
+        // Accessing __reactInternalInstance$
         // https://stackoverflow.com/a/55310101/4058484
         // https://medium.com/@sitambas/get-global-element-state-a408a744e99d
 
@@ -36,7 +36,7 @@ var tree = {
 
         let key = Object.keys(dom).find(key=>key.startsWith("__react"));
         //return getObjectKeys({a: 1, b: 2, c: { d: 3, e: { f: 4 }}});
-        return dom[key];
+        return dom[key]["stateNode"]["current"];
 
     }
 
