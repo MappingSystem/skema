860e13fb48f18ae499cbdfcc85440b048ad53030
Chetabahana <chetabahana@gmail.com>
2019-11-20 17:01:56 +0700

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -6,8 +6,9 @@ var tree = {
         // https://szhshp.org/tech/2019/08/10/reactindepthrender.html
         // https://dev.to/carlmungazi/a-journey-through-reactdom-render-302c
 
-        var root = $('#graphiql')._reactRootContainer._internalRoot;
-        let key = Object.keys($('#graphiql')).find(key=>key.startsWith("__reactInternalInstance$"));
+        var graphiql = document.getElementById('graphiql');
+        var root = graphiql._reactRootContainer._internalRoot;
+        let key = Object.keys(graphiql).find(key=>key.startsWith("__reactInternalInstance$"));
         console.dir(root.current); console.dir(key);
         draw.getJSON();
 
