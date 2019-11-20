979316755f792ecc9fad2fc68df833ab5d9f1c1f
Chetabahana <chetabahana@gmail.com>
2019-11-20 18:08:19 +0700

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -6,28 +6,6 @@ var tree = {
         console.dir(document.getElementById('graphiql')._reactRootContainer._internalRoot);
         draw.getJSON();
 
-    },
-
-    getReactElement : function(dom) {
-
-        // Accessing global element state
-        // https://medium.com/@sitambas/get-global-element-state-a408a744e99d
-        // let key = Object.keys(graphiql).find(key=>key.startsWith("__reactInternalInstance$"));
-
-        for (var key in dom) {
-            
-            if (key.startsWith(“__reactInternalInstance$”)) 
-            {
-               var compInternals = dom[key]._currentElement;
-               var compWrapper = compInternals._owner;
-               var comp = compWrapper._instance;
-               return comp;
-            }
-
-        }
-
-        return null;
-
     }
 
 }
