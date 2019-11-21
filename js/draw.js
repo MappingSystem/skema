83973bdb05b16c8a8047e47d1576d0946cb1b885
Chetabahana <chetabahana@gmail.com>
2019-11-21 21:07:26 +0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -327,8 +327,8 @@ var id, js, ids, pad, back, data, feed, json, link, size, test, type, query, sel
 
         button.attr('title',title);
         button.click(function() {draw.click($('.eQuery').first());});  
-
-        e.mouseup(function(){draw.loading();});
+ 
+        e.mouseup(_.debounce(function(){draw.loading();}, 100));
         $(path).attr('class','eQuery');
         return $(path);
 
