cd19dac1bd098a158b759ca9fe2e3adc8434dd6d
Chetabahana <chetabahana@gmail.com>
2019-11-21 19:59:43 +0700

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -9,7 +9,7 @@ var dom, keys, node, root, element, tree = {
         root = element._reactRootContainer._internalRoot;
 
         dom = element.getElementsByClassName('execute-button')[1];
-        node = this.getReact(); console.log(node._onClick);
+        node = this.getReact(); console.log(node.props.onRun);
 
         draw.getJSON();
 
